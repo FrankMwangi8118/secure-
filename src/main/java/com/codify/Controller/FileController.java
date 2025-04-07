173cc38909fe745e365b67fa51ffbb97b46c8515
CODIFY <frankmwangi8118@gmail.com>
2025-04-07 10:53:51 +0300

    fix: fixed the return on th upload api

--- src/main/java/com/codify/Controller/FileController.java
+++ src/main/java/com/codify/Controller/FileController.java
@@ -2,17 +2,17 @@ package com.codify.Controller;
 
 import com.codify.Commons.ApiResponse;
 import com.codify.Service.FileUploadAndDownload.FileServicesImpl;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.http2.Http2AsyncUpgradeHandler;
+import org.slf4j.Logger;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -20,6 +20,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 
+@Slf4j
 @RestController
 public class FileController {
 
@@ -28,12 +29,13 @@ public class FileController {
     public FileController(FileServicesImpl fileServicesImpl) {
         this.fileServicesImpl = fileServicesImpl;
     }
+
     @PostMapping("/upload")
 
     public ResponseEntity<ApiResponse> uploadFile(@RequestParam MultipartFile file) throws IOException {
         try {
 
-
+            log.info("file");
             return ResponseEntity.ok(
                     ApiResponse.builder()
                             .status("200")
@@ -41,6 +43,7 @@ public class FileController {
                             .build()
             );
         } catch (Exception e) {
+            log.error("error: ", e);
             return ResponseEntity.internalServerError().body(
                     ApiResponse.builder()
                             .status("500")
@@ -50,27 +53,22 @@ public class FileController {
         }
 
     }
-    @GetMapping("/download")
-     public ResponseEntity<ApiResponse> downloadHashedFile(@RequestParam ("file") String fileName) throws IOException {
-        var fileTodownload=fileServicesImpl.downloadFile(fileName);
+
+    @GetMapping("/download/{file}")
+    public ResponseEntity<Resource> downloadHashedFile(@PathVariable("file") String file) throws IOException {
+        var fileTodownload = fileServicesImpl.downloadFile(file);
+
         try {
-            return ResponseEntity.ok(
-                    ApiResponse.builder()
-                            .status("200")
-                            .message("download complete")
-                            .data(ResponseEntity.ok()
-                                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+            return ResponseEntity.ok()
+                                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file + "\"")
                                     .contentLength(fileTodownload.length())
                                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                                    .body(new FileSystemResource(fileTodownload))).build()
-            );
-        }catch (Exception e){
-            return  ResponseEntity.internalServerError().body(
-                    ApiResponse.builder()
-                            .status("500")
-                            .message(e.getMessage())
-                            .build()
-            );
+                                    .body(new FileSystemResource(fileTodownload));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
 
-}}
+    }
+
+    }
+
--- src/main/java/com/codify/Service/FileUploadAndDownload/FileServicesImpl.java
+++ src/main/java/com/codify/Service/FileUploadAndDownload/FileServicesImpl.java
@@ -2,6 +2,7 @@ package com.codify.Service.FileUploadAndDownload;
 
 import com.codify.Service.ImageHashing.ImageHashingImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,46 +16,53 @@ import java.util.Arrays;
 
 @Service
 
-@AllArgsConstructor
 public class FileServicesImpl implements FileService {
     public final ImageHashingImpl imageHashingImpl;
-    public static final String DIR="./files";
+    public static final String DIR = "./files";
+    @Value("${download.path}")
+  public final String downloadPath;
+
+    public FileServicesImpl(ImageHashingImpl imageHashingImpl, @Value("${download.path}") String downloadPath) {
+        this.imageHashingImpl = imageHashingImpl;
+        this.downloadPath = downloadPath;
+    }
 
 
     @Override
     public String uploadFileToDir(MultipartFile file) throws IOException {
-        if(file==null){
+
+        if (file == null) {
             throw new NullPointerException("null files aint allowed");
         }
-        var targetFile=new File(DIR+File.separator+file.getOriginalFilename());
+        var targetFile = new File(DIR + File.separator + file.getOriginalFilename());
 
 //        if(!(Objects.equals(targetFile.getParentFile(),DIR))){
 //            throw new SecurityException("unsupported file name");
 //        }
 
-        File fileDir=new File(DIR);
+        File fileDir = new File(DIR);
 
-        if (!fileDir.exists()){
+        if (!fileDir.exists()) {
             fileDir.mkdir();
         }
-        byte[] imageBytes=Files.readAllBytes(Path.of( DIR + File.separator + file.getOriginalFilename()));
+        Files.copy(file.getInputStream(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        final String filePath = DIR + File.separator + file.getOriginalFilename();
+
+        byte[] imageBytes = Files.readAllBytes(Path.of(DIR + File.separator + file.getOriginalFilename()));
         imageHashingImpl.hashEngine(imageBytes);
-        System.out.println(Arrays.toString(imageBytes));
 
-        Files.copy(file.getInputStream(),targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-
-        return "file upload and hashing completed";
+        return downloadPath + File.separator + file.getOriginalFilename();
     }
 
     @Override
     public File downloadFile(String fileName) throws FileNotFoundException {
-        if(fileName==null || fileName.isEmpty()){
+        if (fileName == null || fileName.isEmpty()) {
             throw new NullPointerException();
         }
-        var fileToDownload=new File(DIR+File.separator+fileName);
-        if(!fileToDownload.exists()){
-            throw new FileNotFoundException("no file named ...."+fileName);
+        var fileToDownload = new File(DIR + File.separator + fileName);
+        if (!fileToDownload.exists()) {
+            throw new FileNotFoundException("no file named ...." + fileName);
         }
         return fileToDownload;
     }
